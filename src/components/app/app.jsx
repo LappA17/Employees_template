9cc504c2fecf440d8e42b43c791abfbe6c8960bb
Ruslan <ruslan.postoyuk@gmail.com>
2022-02-16 22:03:49 +0100

    93. Работа со списками и алгоритм согласования

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,17 +8,12 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
-/* Попрактикуемся поработать с даными (переменная data)
-
-   Сейчас добавиим класс increase что бы в случае появляение его у элемента - элемент на странице менял цвет, 
-пока что не будем добавлять обрабртчик события, но логика такая что при клике на печеньку - нам будет оранжевым
-подсвечивать человека который идет на повышение*/
 function App() {
 
     const data = [
-        {name: 'Ruslan P', salary: 6000, increase: false},
-        {name: 'Liza S', salary: 3000, increase: true},
-        {name: 'Edward D', salary: 6500, increase: false}
+        {name: 'Ruslan P', salary: 6000, increase: false, id: 1},
+        {name: 'Liza S', salary: 3000, increase: true, id: 2},
+        {name: 'Edward D', salary: 6500, increase: false, id: 3}
     ]
 
     return(
@@ -33,6 +28,6 @@ function App() {
             <EmployersAddForm/>
         </div>
     ); 
-} /* Передаем в Компонент нашу переменую которая содержит массив данных */
+}
 
 export default App;
