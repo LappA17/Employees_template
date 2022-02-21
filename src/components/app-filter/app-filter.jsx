f9e995d112b6d89359696a01832dac781b18a359
Ruslan <ruslan.postoyuk@gmail.com>
2022-02-21 23:28:17 +0100

    103. Семантика и доступность контента

--- src/components/app-filter/app-filter.jsx
+++ src/components/app-filter/app-filter.jsx
@@ -1,45 +1,21 @@
-// Фильтер сотрудников
-import './app-filter.css';
-
-/* Ваня сказал что в качестве учебного материала он хочет показать еще один способ добавляения фильтра. Дело в том что когда у нас
-кнопок много(особенно фильтров), то они нам приходят чаще не в качестве уже готовой верстке(например 10 одинаковых кнопок), а они
-чаще всего рассположены в каком-то массиве с данными(так даже удобней будет работать когда кнопки формируются из массива с данными) и уже
-потом будет интерфейс. И мы создаем buttonsData 
-
-    Обрати внимание что у одной кнопки отличается класс btn-light вместо outline. У нас btn-outline-light подставляется тогда,
-когда кнопка - неактивна. А btn-light когда активна и у нас эта кнопка будет в массиве, по этому на данные мы не можем ориентироваться.
-Единственное на что мы можем ориентироваться это на ФИЛЬТЕР который нам приходит из app.jsx. Мы назначаем в app.jsx в filter: 'all'
-что бы уже передавать фильтер в компонента app-filter и уже здесь использовать и уже от этого будет зависить какая кнопка будет активна.
-ДЛЯ ЭТОГО В app.jsx в AppFilter как проперти filter буду передавать текущий стейт. И у меня получается что теперь в props.filter лежит
-название фильтра КОТОРОЕ СОВПАДАЕТ С ОДНИМ ИЗ НАШИХ name(в данном случае name: "all"). По-этому создаем переменную active*/
+import "./app-filter.css";
 
 const AppFilter = (props) => {
     const buttonsData = [
-        {name: "all", label: 'Wszyscy pracownicy'}, // name - filter с app.jsx, label - это то что будет писаться внутри этой кнопки 
-        {name: "rise", label: 'Pracownicy do awansu'},
-        {name: "moreThen4000", label: 'Wynagrodzenie powyżej 4000 zł'}
-    ]
+        {name: 'all', label: 'Все сотрудники'},
+        {name: 'rise', label: 'На повышение'},
+        {name: 'moreThen1000', label: 'З/П больше 1000$'}
+    ];
 
-    /* В map у нас будет каждый отдельный объект из которого я буду вытаскивать нужные мне данные  */
     const buttons = buttonsData.map(({name, label}) => {
-        const active = props.filter === name/* Так как у нас метод map проходится по всем объектам, то я буду узнавать 
-активный это объект или нет, то-есть где совпадает у нас фильтр. Эта запись аналогична записи if и в active у нас будет либо true либо false*/
-        const clazz = active ? 'btn-light' : 'btn-outline-light'/* Создадим переменую которая будет содержать в себе строчку с классом !
-Если active тру то назначаем класс бтн-лайт, если фолс то оутлайн */
-/* В бектиках назначаем нужный нам класс и теперь при переборе когда мы натыкаемся на действительно активный который совпадает с неймом
-то мы эту кнопку будет подсвечивать как активную ! */
-
-    /* Здесь что бы активировать onFilterSelect нужно обратиться к пропсам и активировать обработчик событий. Но если бы все наши кнопки
-были бы статичны то мы бы просто на каждую кнопку cntr c + cntr v навесили бы обработчики событий. Но так как у нас все происходит
-в одном месте то мы вызываем onClick и прям там же вызываем коллбек. Передаем аргумент name потому что это название нашего фильтра
-который мы туда будем передавать*/
+        const active = props.filter === name;
+        const clazz = active ? 'btn-light' : 'btn-outline-light';
         return (
-            <button 
-                className={`btn ${clazz}`}
-                type="button"
-                key={name}
-                onClick={() => props.onFilterSelect(name)}>
-                {label}
+            <button type="button"
+                    className={`btn ${clazz}`}
+                    key={name}
+                    onClick={() => props.onFilterSelect(name)}>
+                    {label}
             </button>
         )
     })
@@ -47,23 +23,8 @@ const AppFilter = (props) => {
     return (
         <div className="btn-group">
             {buttons}
-            {/* <button 
-                className="btn btn-light"
-                type="button">
-                    Wszyscy pracownicy
-            </button>
-            <button 
-                className="btn btn-outline-light"
-                type="button">
-                    Pracownicy do awansu
-            </button>
-            <button 
-                className="btn btn-outline-light"
-                type="button">
-                    Wynagrodzenie powyżej 4000 zł 
-            </button> */}
         </div>
-    ) 
+    )
 }
 
 export default AppFilter;
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,104 +1,78 @@
-// Это будет папка и файл который будет объединять все наши компоненты !
-
 import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
-import EmployersList from '../employers-list/employers-list';
-import EmployersAddForm from '../employers-add-form/employers-add-form';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-/* Будем фильтровать по имени пользователей, и посел фильтрации остануться только ты пользователи которых нужно отобразить
-на страничке */
 class App extends Component {
-    constructor(props){
-        super(props)
+    constructor(props) {
+        super(props);
         this.state = {
             data: [
-                {name: 'Ruslan P', salary: 6000, increase: false, rise: true, id: 1},
-                {name: 'Liza S', salary: 3000, increase: true, rise: false, id: 2},
-                {name: 'Edward D', salary: 6500, increase: false, rise: false, id: 3}
+                {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
             ],
-            term: '', // Данную строку будет невручную писать, а в компонента search-panel
+            term: '', 
             filter: 'all'
         }
-        this.maxId = 4
-    } 
+        this.maxId = 4;
+    }
 
     deleteItem = (id) => {
         this.setState(({data}) => {
-            // 1ый способ
-            //const index = data.findIndex(elem => elem.id === id); 
-            //const before = data.slice(0, index)   
-            //const after = data.slice(index + 1) 
-            /*const newArr = [...before, ...after]  
-            /* return {
-                data: newArr // вместо data возвращаем новый массив 
-            } */
-
-    // 2) Способ filter - создавая новый массив
             return {
                 data: data.filter(item => item.id !== id)
-            } 
+            }
         })
     }
 
+    // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
     addItem = (name, salary) => {
         const newItem = {
-            name,
+            name, 
             salary,
             increase: false,
             rise: false,
-            id: this.maxId++ 
+            id: this.maxId++
         }
         this.setState(({data}) => {
-            const newArr = [...data, newItem]; 
+            const newArr = [...data, newItem];
             return {
                 data: newArr
             }
         });
     }
 
-    onToggleProp = (id, prop) => { 
-        this.setState(({data}) => ({ 
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
             data: data.map(item => {
                 if (item.id === id) {
                     return {...item, [prop]: !item[prop]}
                 }
-                return item
+                return item;
             })
         }))
-    } 
+    }
 
-    // Метод для поиска
     searchEmp = (items, term) => {
-        if (term.length === 0) { // Если пользователь ничего не введет в поиск
-            return items // То мы ничего делать не будем а просто вернем тот массив который нам прийдет
+        if (term.length === 0) {
+            return items;
         }
 
-        // Если условие выше не сработает
         return items.filter(item => {
             return item.name.indexOf(term) > -1
         })
     }
-/* Второй аргумент - это строчка по которой будем исать. Первый аргумент - это массив данных который мы будем фильтровать 
-   Мы будет фильтровать массив таким образом что будем брать свойство name в объекте data и искать сходство с term(то где пользовать
-вбивает в поиске)
-   Мы два раза вписываем return потому что у нас двойная КОЛЛБЕК функция
-   indexOf - позволяет искать строки или подстроки. И если indexOf ничего не найдет то он будет равен -1. Поэтому прописываем что
-наш поиск должен быть чему-то равен большему чем -1 
-    На этом этапе когда мы уже передали стейты в наш рендер, если я в term ввиду какуе-то букву то у меня будет на страничке 
-только тот работник у которого есть данная строка в имени*/
 
-    // Обновление нашего поиска. Этот метод будет отвечать за установления состояние внутри нашего главного компонента app.jsx
     onUpdateSearch = (term) => {
         this.setState({term});
     }
-    /* Этот метод будет только устанавливать состояние
-    ОЧЕНЬ ВАЖНО ({term: term}) РАВНО ТОЖЕ САМОМУ ЧТО И ЭТО ({term})  */
 
-    // Займемся нашим флиьтром
     filterPost = (items, filter) => {
         switch (filter) {
             case 'rise':
@@ -106,66 +80,37 @@ class App extends Component {
             case 'moreThen4000':
                 return items.filter(item => item.salary > 4000);
             default:
-                return items;
+                return items
         }
     }
-/* Первый аргумент - наш массив, Второй - фильтер по которому мы будем фильтровать
-   Так как у нас от фильтра можем меняться несколько позиции(Все сотрудники, Працовницы до авансу и вынагродзенье повыжей 4000), по-этому
-нам нужно взять наш filter и взависимости от его содержания что-то с ним сделать.
-   Так как у нас три значения фильтра - будем использовать конструкцию switch 
 
-   1)case 'rise' - тут можно выбрать название строчек абсолютно рандомные, не обязательно rise 
-   И если наш фильтер будет rise, то мы применяем этот участок кода return items.filter(item => item.rise), нужно напомнить что здесь
-item.rise стоит в логическом контексте true , это тоже самое что return items.filter(item => if (item.rise) return;. Весь этот код
-значит что если в нашем массиве data будет объект где rise стоит в позиции true - то он будет включен в наш массив
-    Здесь break можно не ставить потому что React и сам знает !
-
-    2) Те у кого зп больше 4000 злотых return items.filter(item => item.salary > 4000);
-
-    3) Это default (если ни один из фильтров не сработает), те все сотрудники. То мы просто возвращаем наш массив
-    */
-
-    // Теперь необходимо что бы динамически фильтр переключался. (Обрати внимание что все метод который может сделать пользователь Ваня начинает с on, а статически методы уже без)
     onFilterSelect = (filter) => {
-        this.setState({filter})
+        this.setState({filter});
     }
-    /* Мы изменяем в нашем состояние текущий фильтер */
-
 
     render() {
         const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
-        // Создадим отфильтрованный после поиска массив data. И уже потом передаем visibleData как data(данные) в EmployersList
-        // const visibleData = this.searchEmp(data, term); Мы использовали эти пропсы для поиска, но Ваня сказал что можно немного передалать и использовать так же и для фильтров
-        const visibleData = this.filterPost(this.searchEmp(data, term), filter); /* У нас метод фильтерПост принимает в себя два аргумент:
-массив и фильтер. и как массив мы в него помещаем this.searchEmp(data, term)(то что было в нашем поиске), то-есть мы будем фильтровать
-уже как бы отфильтрованный массив. А filter мы берем с нашего this.state. То-есть наши данные которые будут отображаться будут проходить
-две фильтрации: сначало по поиску, потом по фильтру */
-
-        return(
+        return (
             <div className="app">
-                <AppInfo
-                    employees={employees}
-                    increased={increased}/>
+                <AppInfo employees={employees} increased={increased}/>
     
                 <div className="search-panel">
-                    <SearchPanel
-                        onUpdateSearch={this.onUpdateSearch}/>
-                    <AppFilter
-                        filter={filter}
-                        onFilterSelect={this.onFilterSelect}/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
-                <EmployersList 
+                
+                <EmployeesList 
                     data={visibleData}
-                    onDelete={this.deleteItem} 
+                    onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
-                <EmployersAddForm
-                    onAdd={this.addItem}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
-        )
-    } 
+        );
+    }
 }
 
 export default App;
