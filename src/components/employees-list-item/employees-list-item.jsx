c76b489affdc08f79488c0aeeced0d757a9944c6
Ruslan <ruslan.postoyuk@gmail.com>
2022-02-22 22:37:29 +0100

    105. Стили в React. CSS и SASS/SCSS

--- src/components/employees-list-item/employees-list-item.jsx
+++ src/components/employees-list-item/employees-list-item.jsx
@@ -12,20 +12,12 @@ const EmployeesListItem = (props) => {
         classNames += ' like';
     }
 
-    /* В каждой компании свой подход работы со стилями, кто-то больше уперается в инлайн стили, кто-то css(sass), и тд.
-Ваня сказал что нет особо разницы, каждый работает так как ему удобней, но он покажет примеры по каждым стилям
-style={{fontSize: 40}} - задаем в виде Объекта инлайн-стили + важно что мы недописываем к 40 пиксели ! реакт понимает
-за нас это. Реакт подставляет только пиксели.
-    Так же этот объект можно вынести в переменную и передавать style={{fontSize: 40, color: 'orange'}} 
-    Мы не можем просто прописать transition: 'all', потому что нужно будет ВРУЧНУЮ указывать префиксы к нему
-WebkitTransitiom: 'all' или msTransition: 'all'. За это нужно помнить, это такой минус Реакта*/
     return (
         <li className={classNames}>
             <span  
                 className="list-group-item-label" 
                 onClick={onToggleProp} 
                 data-toggle="rise"
-                style={{fontSize: 40, color: 'orange'}}
                 >{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + 'zł'}/>
             <div className='d-flex justify-content-center align-items-center'>
